0d360c69d33efbac71d20ea4d6e82e30af8658b6
Rushikesh Wani <wanirushikeshanil11@gmail.com>
2024-12-12 20:06:59 +0530

    added connection to live mongoDB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  const options = {
+    dbName: "cropZy",
+  };
+  await mongoose.connect(process.env.MONGO_URI, options);
 };
 
 module.exports = { connectDB };
